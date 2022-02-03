265ffe1b4ba58ede9988cae09c7ce9e22ca6029d
asadkhan-786-gb <m.asadkhan079@gmail.com>
2022-02-03 15:57:49 +0500

    work Accomplish

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball Game");
+        obj.setResizable(false);
+        obj.setVisible(true);
+
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+    }
+}
